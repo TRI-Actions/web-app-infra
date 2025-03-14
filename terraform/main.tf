a4dac57911a2f802eaba263295a6135a8e7f9760
Zachary Ables <zach.ables.ctr@tri.global>
2025-03-14 16:03:25 +0000

    sns-topic

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,24 +1,3 @@
-#tfsec:ignore:aws-cloudtrail-require-bucket-access-logging
-module "s3" {
-  source      = "git@github.com:TRI-IE/tf-s3-bucket.git?ref=0.1.3"
-  bucket_name = "zables-test-bucket"
-
-  bucket_policy = <<EOT
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "test",
-            "Effect": "Allow",
-            "Principal": {
-                "Service": [
-                    "cloudtrail.amazonaws.com"
-                ]
-            },
-            "Action": "*",
-            "Resource": "*",
-        }
-    ]
-  }
-EOT
+resource "aws_sns_topic" "user_updates" {
+  name = "test-topic"
 }
